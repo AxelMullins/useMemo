cbb7f09d9bdc909f750f1d08fcc2b12be2b9e1e2
Axel Mullins <axelmullins94@gmail.com>
2022-04-25 17:01:01 -0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 function App() {
+  const [userNumber, setUserNumber] = useState("");
+  const [randomInput, setRandomInput] = useState("");
+
+  // Memoriza la funcion
+  const fib = useCallback((num) => {
+    return num <= 1 ? num : fib(num - 1) + fib(num - 2);
+  }, []);
+
+  // Memoriza el resultado de la función para no tener que re-calcularlo todo el tiempo
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
+
+  useEffect(() => {
+    console.log("New number");
+  }, [fibNumber]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>useMemo</h1>
+      <section>
+        <h2>Fibonacci Sequence</h2>
+        <input
+          type="number"
+          name="userNumber"
+          id="userNumber"
+          value={userNumber}
+          onChange={(e) => setUserNumber(e.target.value)}
+        />
+        <p>Number: {fibNumber || "--"}</p>
+      </section>
+      <br />
+      <section>
+        <input
+          type="text"
+          name="test"
+          id="text"
+          value={randomInput}
+          onChange={(e) => setRandomInput(e.target.value)}
+        />
+        <p>{randomInput}</p>
+      </section>
     </div>
   );
 }
